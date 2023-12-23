5437e6db7dd4d96bfb0923a16ee17658fa60f76c
Arthur Moreira de Deus <arthur.md@parade.ai>
2023-12-23 11:53:15 -0300

    ci: Update CI workflow

--- settings/settings.py
+++ settings/settings.py
@@ -87,8 +87,8 @@ WSGI_APPLICATION = "settings.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DB_NAME", default="template-api-db"),
-        "USER": env("DB_USER", default="template-api"),
+        "NAME": env("DB_NAME", default="budget-api-db"),
+        "USER": env("DB_USER", default="budget-api"),
         "PASSWORD": env("DB_PASSWORD", default="badaras"),
         "HOST": env("DB_HOST", default="localhost"),
         "PORT": env("DB_PORT", default=5432),
